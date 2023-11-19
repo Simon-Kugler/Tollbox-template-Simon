2d268013d26e5a3008866e79daaa783536e89743
Simon Kugler <simon.kugler@tu-dortmund.de>
2023-11-19 01:06:31 +0100

    Grobe Fertigstellung

--- v203/Python/plotd.py
+++ v203/Python/plotd.py
@@ -1,25 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
 
-p, T = np.genfromtxt("../Messdaten2_203.txt", unpack=True)
+T, p = np.genfromtxt("../Messdaten2_203.txt", unpack=True)
 
-# Fit a polynomial of degree 1, return covariance matrix
-params, covariance_matrix = np.polyfit(p, T, deg=4, cov=True)
-
+# Fit a polynomial of degree 4, return covariance matrix
+params, covariance_matrix = np.polyfit(T, p, deg=3, cov=True)
+# Fehllerrechnung
 errors = np.sqrt(np.diag(covariance_matrix))
 
-for name, value, error in zip("abcde", params, errors):
+for name, value, error in zip("abcd", params, errors):
     print(f"{name} = {value:.15f} ± {error:.15f}")
 
-fig, ax = plt.subplots(1, layout="constrained")
+fig, ax = plt.subplots(1,1,layout="constrained")
 
-x_T=np.linspace(390,466,10000)
-ax.plot(p, T, "r", label="Messdaten")
+x_p=np.linspace(390,466,10000)
+ax.plot(T, p, "r.", label="Messdaten")
 ax.plot(
-    x_T,
-    params[0]*p**4+params[1]*p**3+params[2]*p**2+params[3]*p+params[4],
+    x_p,
+    params[0]*x_p**3+params[1]*x_p**2+params[2]*x_p**1+params[3],
     label="Regression"
 )
+ax.set_ylabel("T / K")
+ax.set_xlabel("p / Pa")
 ax.grid()
 ax.legend()
 
