2c6e68cd2ac9c2d224922e68564cdfe8cc072180
Simon Kugler <simon.kugler@tu-dortmund.de>
2023-11-21 14:59:44 +0100

    fertig

--- v203/plotd.py
+++ v203/plotd.py
@@ -1,17 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
-
+from uncertainties import ufloat
 T, p = np.genfromtxt("Messdaten2_203.txt", unpack=True)
 
-# Fit a polynomial of degree 4, return covariance matrix
+# Fit a polynomial of degree 3, return covariance matrix
 params, covariance_matrix = np.polyfit(T, p, deg=3, cov=True)
 # Fehllerrechnung
 errors = np.sqrt(np.diag(covariance_matrix))
 
 for name, value, error in zip("abcd", params, errors):
     print(f"{name} = {value:.15f} ± {error:.15f}")
-
 fig, ax = plt.subplots(1,1,layout="constrained")
 
 x_p=np.linspace(390,466,10000)
