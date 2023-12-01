178d0e1aa83d1bcdd81c9db2b0c5475b0b3f1e23
sophiabrmn <sophia.brechmann@tu-dortmund.de>
2023-12-01 16:49:12 +0100

    Winkelrichtgröße ausgerechnet und Auswertung dazu geschrieben.

--- v101/Python/plota.py
+++ v101/Python/plota.py
@@ -2,19 +2,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
-phi, F = np.genfromtxt("../Messwerte/Winkelrichtgroesse.txt", unpack=True)
+Phi, F = np.genfromtxt("../Messwerte/Winkelrichtgroesse.txt", unpack=True)
+D = np.zeros(10)
+# phi in rad
+Phin = Phi * np.pi / 180
 
-phil = len(phi)
+Phil = len(Phin)
 Fl = len(F)
+Dl = len(D)
 
 #Mittelwerte bestimmen
 Fx = 0
-phix = 0
-for x in range(0,len(phi)):
-    phix += phi[x]
+Phix = 0
+Dx = 0
 
-for y in range(0,len(F)):
+D = np.zeros(10)
+for f, phi, i in zip(F, Phin, range(0, Phil)):
+    D[i] = f * 0.2 / phi
+
+for x in range(0, Phil):
+    Phix += Phin[x]
+
+for y in range(0, Fl):
     Fx += F[y]
 
-print("Mittelwert von Phi =", phix/phil)
+for d in range(0, Dl):
+    Dx += D[d]
+print("Mittelwert von Phi =", Phix/Phil)
 print("Mittelwert von F = ", Fx/Fl)
+print("Mittelwert von D = ", Dx/Dl)
+
+
+
+
