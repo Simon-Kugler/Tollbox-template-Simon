58829d5dd4a70dd3ac977f6f6117cc34eb4e72d2
Simon Kugler <simon.kugler@tu-dortmund.de>
2023-12-18 08:36:53 +0100

    paar Sachen

--- v206/python/plota.py
+++ v206/python/plota.py
@@ -45,41 +45,51 @@ fig.savefig("build/plota.pdf")
 #Ableitung: 2At+B
 print("")
 print("Differenzenqutient 1 bei t=5")
-print(2*params1[0]*5+params1[1])
+dT_11dt=2*params1[0]*5*60+params1[1]
+print(dT_11dt)
 print("Differenzenqutient 1 bei t=10")
-print(2*params1[0]*10+params1[1])
+dT_12dt=2*params1[0]*10*60+params1[1]
+print(dT_12dt)
 print("Differenzenqutient 1 bei t=15")
-print(2*params1[0]*15+params1[1])
+dT_13dt=2*params1[0]*15*60+params1[1]
+print(dT_13dt)
 print("Differenzenqutient 1 bei t=20")
-print(2*params1[0]*20+params1[1])
+dT_14dt=2*params1[0]*20*60+params1[1]
+print(dT_14dt)
 
+dT1dt=np.array([dT_11dt,dT_12dt,dT_13dt,dT_14dt])
 print("")
 print("Differenzenqutient 2 bei t=5")
-print(2*params2[0]*5+params2[1])
+dT_21dt=2*params2[0]*5*60+params2[1]
+print(dT_21dt)
 print("Differenzenqutient 2 bei t=10")
-print(2*params2[0]*10+params2[1])
+dT_22dt=2*params2[0]*10*60+params2[1]
+print(dT_22dt)
 print("Differenzenqutient 2 bei t=15")
-print(2*params2[0]*15+params2[1])
+dT_23dt=2*params2[0]*15*60+params2[1]
+print(dT_23dt)
 print("Differenzenqutient 2 bei t=20")
-print(2*params2[0]*20*60+params2[1], "+/-", 2*errors2[0]*20*60+errors2[1])
+dT_24dt=2*params2[0]*20*60+params2[1]
+print(dT_24dt)
 
+dT2dt=np.array([dT_21dt,dT_22dt,dT_23dt,dT_24dt])
 
-dT1dt = [0, 0, 0, 0, 0]
-dT2dt = [0, 0, 0, 0, 0]
-dT1dte = [0, 0, 0, 0, 0]
-dT2dte = [0, 0, 0, 0, 0]
-for i in range (1, 5, 1):
-    dT1dt[i] = 2*params1[0]*i*5*60+params1[1]
-    print(5*i)
-
-for i in range (1, 5, 1):
-    dT2dt[i] = 2*params2[0]*i*5*60+params2[1]
-
-for i in range (1, 5, 1):
-    dT1dte[i] = 2*errors1[0]*i*5*60+errors1[1]
-
-for i in range (1, 5, 1):
-    dT2dte[i] = 2*errors2[0]*i*5*60+errors2[1]
+# dT1dt = [0, 0, 0, 0, 0]
+# dT2dt = [0, 0, 0, 0, 0]
+# dT1dte = [0, 0, 0, 0, 0]
+# dT2dte = [0, 0, 0, 0, 0]
+# for i in range (1, 5, 1):
+    # dT1dt[i] = 2*params1[0]*i*5*60+params1[1]
+    # print(5*i)
+# 
+# for i in range (1, 5, 1):
+    # dT2dt[i] = 2*params2[0]*i*5*60+params2[1]
+# 
+# for i in range (1, 5, 1):
+    # dT1dte[i] = 2*errors1[0]*i*5*60+errors1[1]
+# 
+# for i in range (1, 5, 1):
+    # dT2dte[i] = 2*errors2[0]*i*5*60+errors2[1]
 
 
 v_real = [0, 0, 0, 0]
@@ -87,26 +97,26 @@ N = [200.0, 206.0, 210.0, 205.0]
 m_1 = m_2 = 3.0
 mc_k = 750.0
 c_w = 4200.0
-print("dT1dt", dT1dt)
-print("dT1dte", dT1dte)
-print("dT2dt", dT2dt)
-print("dT2dte", dT2dte)
+# print("dT1dt", dT1dt)
+# print("dT1dte", dT1dte)
+# print("dT2dt", dT2dt)
+# print("dT2dte", dT2dte)
 
-# print("T1 reale Gueteziffer")
-# for i in range (0, 4, 1):
-#    v_real[i] = (1/N[i]) * (m_1 * c_w + mc_k) * dT1dt
-#    print(v_real[i])
-# print("T2 reale Gueteziffer")
-# for i in range (0, 4, 1):
-#    v_real[i] = 1/N[i] * (m_1 * c_w + mc_k) * dT2dt
-#    print(v_real[i])
-# 
-# v_ideal = [0, 0, 0, 0, 0]
-# 
-# print("ideale Gueteziffer:")
-# for i in range (1, 5, 1):
-    # v_ideal[i] = (T_b[5*i])/(T_b[5*i] - T_a[5*i])
-    # print(5*i, v_ideal[i])
+print("T1 reale Gueteziffer")
+for i in range (0, 4, 1):
+   v_real[i] = (1/N[i]) * (m_1 * c_w + mc_k) * dT1dt
+   print(v_real[i])
+print("T2 reale Gueteziffer")
+for i in range (0, 4, 1):
+   v_real[i] = 1/N[i] * (m_1 * c_w + mc_k) * dT2dt
+   print(v_real[i])
+
+v_ideal = [0, 0, 0, 0, 0]
+
+print("ideale Gueteziffer:")
+for i in range (1, 5, 1):
+    v_ideal[i] = (T_a[5*i]+273.15)/(T_a[5*i]+273.15 - T_b[5*i]-273.15)
+    print(5*i, v_ideal[i])
 
 m =ufloat(2726.2816288047175,179.00481892007065)
 b =ufloat(4.1229755871964695,0.5793950001616602)
@@ -115,10 +125,10 @@ L2=ufloat(22700,1500)
 L2=L2/120.913
 #Massendurchsatz bestimmen
 #dQ_2/dt=(m_1c_w+m_kc_k)dT_1/dT
-Qt1=(m_1*c_w+mc_k)*ufloat(dT2dt[1],dT2dte[1])/L2
-Qt2=(m_1*c_w+mc_k)*ufloat(dT2dt[2],dT2dte[2])/L2
-Qt3=(m_1*c_w+mc_k)*ufloat(dT2dt[3],dT2dte[3])/L2
-Qt4=(m_1*c_w+mc_k)*ufloat(dT2dt[4],dT2dte[4])/L2
+Qt1=(m_1*c_w+mc_k)*dT_21dt/L2
+Qt2=(m_1*c_w+mc_k)*dT_22dt/L2
+Qt3=(m_1*c_w+mc_k)*dT_23dt/L2
+Qt4=(m_1*c_w+mc_k)*dT_24dt/L2
 print("Qt1", Qt1)
 print("Qt2", Qt2)
 print("Qt3", Qt3)
