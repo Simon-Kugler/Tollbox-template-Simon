e6fcb280cdb419a03969bc3e62c5d77f48deb93c
Simon Kugler <simon.kugler@tu-dortmund.de>
2024-01-04 13:23:32 +0100

    Dateien für 353

--- v353/python/plotc.py
+++ v353/python/plotc.py
@@ -4,31 +4,23 @@ from uncertainties import ufloat
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
-f, U_G, U_C2, a, b, b2= np.genfromtxt("../messdaten/messdaten2.txt", unpack=True)
+f, U_G, U_C2, a, b, b2= np.genfromtxt("messdaten/messdaten2.txt", unpack=True)
 x_t=np.linspace(0,5000)
-phi=a/b2*np.pi
-# print(phi)
-def x2(f,R):
+phi=a/b*np.pi
+print(phi)
+def x2(f):
     w=2*np.pi*f
-    return 1/(np.sqrt(1+w**2*R**2))
+    return np.arctan(0.52*w )
 
-params, covariance_matrix = curve_fit(x2,f, phi)
-errors = np.sqrt(np.diag(covariance_matrix))
 
-for name, value, error in zip("R", params, errors):    
-    print(f"{name} = {value:.6f} ± {error:.6f}")
-
-#Berechnen von RC
-R=params[0]
-RC=ufloat(params[0],errors[0])*10**3
-print("RC=",RC)
 # Plotten Messdaten
 fig, ax = plt.subplots(1,layout="constrained")
 ax.plot(f, phi, "rx", label="Messdaten")
 #Plotten Fit
-ax.plot(x_t, x2(x_t,R),label="Regression")
+# U_C1[U_C1 <= 0] = 1e-10
+ax.plot(x_t, x2(x_t),label="Regression")
 ax.set_xlabel(r"$f$ / Hz")
 ax.set_ylabel(r"$\frac{A(\omega)}{U_0}$")
 ax.grid()
 ax.legend()
-fig.savefig("../build/plotc.pdf")
+fig.savefig("build/plotc.pdf")
