e31703c785adbe5be5627a8041572614c0a317b9
Simon Kugler <simon.kugler@tu-dortmund.de>
2024-01-07 15:13:12 +0100

    Müsste fertig sein  oder so?

--- v353/python/plota.py
+++ v353/python/plota.py
@@ -4,7 +4,7 @@ from uncertainties import ufloat
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 #Daten importieren
-t, U_C1 = np.genfromtxt("messdaten/messdaten1.txt", unpack=True)
+t, U_C1= np.genfromtxt("messdaten/messdaten1.txt", unpack=True)
 x_t=np.linspace(0,1.5)
 
 #Anpassen der Position der Kurve
@@ -21,14 +21,15 @@ errors = np.sqrt(np.diag(covariance_matrix))
 for name, value, error in zip("ab", params, errors):    
     print(f"{name} = {value:.3f} ± {error:.3f}")
 
-#Berechnen von RC
+# Berechnen von RC
 RC=1/ufloat(params[0],errors[0])
 print("RC=",RC)
-#Plotten Messdaten
+# Plotten Messdaten
 fig, ax = plt.subplots(1,layout="constrained")
 ax.plot(t, np.log(U_C1/U_0), "rx", label="Messdaten")
-#Plotten Fit
+# Plotten Fit
 ax.plot(x_t, x_t*params[0]+params[1], label="Lineare Regression")
+# ax.plot(x_t, -0.2*x_t-0.5, label="Lineare Regression")
 ax.set_xlabel(r"$t$ / ms")
 ax.set_ylabel(r"$\ln\left(\frac{U_C}{U_0}\right)$")
 ax.grid()
